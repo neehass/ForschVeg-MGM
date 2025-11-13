9a1960e72803ab6a93f94b329c5fe68882cd2d54
neehass <neele.hass@student.kit.edu>
2025-11-13 14:27:18 +0100

    move new final species to input

---
+++ createVirtualSpecies/select_species_groupwise.r
@@ -0,0 +1,9 @@
+# --------------------------------------------
+# select species
+# --------------------------------------------
+# 14xxx oligotroph
+# 15xxx mesotroph
+# 16xxx eutroph
+
+# random selection of certain number (n) per species group
+# --------------------------------------------
